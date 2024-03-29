d2732166c38babe8d1263f8221fe2b065251aa42
RichBryan <bryanli20282gmail.com>
2024-03-29 19:15:39 -0400

    refactor the home page

--- tth/app/page.tsx
+++ tth/app/page.tsx
@@ -1,51 +1,111 @@
-//import Image from "next/image";
-import  styles from "../Styles/Landing.module.css";
-import "../Styles/global.css";
-import '@fontsource/kadwa/700.css';
-import Topbar from "../components/Topbar";
-import Item from "../components/Item";
-import img1 from "../Images/plato_complete_works.jpg"
-import RevHoodie from "../Images/Revenge_Hoodie.webp";
-import img2 from "../Images/Stanley_cup_pic.jpg";
-import img3 from "../Images/sweater_for_Trade.webp";
-import img4 from "../Images/worn_Vans.webp"
-import img5 from "../Images/used_iphone_xr.webp"
-const Rev = RevHoodie.src;
-const Plato = img1.src;
-const StanleyCup = img2.src;
-const Sweater = img3.src;
-const vans = img4.src;
-const XR = img5.src;
-//import Stack from '@mui/material/Stack';
-//import Button from '@mui/material/Button';
+"use client";
+import React, { useState, useEffect } from "react";
+import { HoverEffect } from "../components/ui/card-hover-effect";
+import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
+import Link from "next/link";
 
+interface Project {
+  title: string;
+  description: string;
+  link: string;
+  tag: string;
+}
 
-  function Hub(){
+interface Props {
+  projects: Project[];
+}
 
-    return(
-      <main className={styles.Hello}>
-        <Topbar />
-       <div className={styles.upper_content}>
-          <div className={styles.display_demo}>
-            <Item imageUrl={Plato} item_name="Plato complete works" item_condition="Good Condition"/> 
-            <Item imageUrl={Sweater} item_name="pastel-sweater" item_condition="Never Worn"/> 
-            <Item imageUrl={StanleyCup} item_name="Stanley Cup - white" item_condition="never used"/> 
-            <Item imageUrl={vans} item_name="Low Top Vans - Black" item_condition="Heavily Used"/> 
-            <Item imageUrl={Rev} item_name="Revenge Hoodie Embroidered" item_condition="Never Worn"/> 
-            <Item imageUrl={XR} item_name="Iphone XR - Black" item_condition="used"/> 
-          </div>
-          <div className={styles.slogan}>
-            <h1 id={styles.one}>Trading made <span>Simple</span>,</h1>
-            <h1 id={styles.two}>Trading made <span>Safe</span>,</h1>
-            <h1 id={styles.three}>Trading made <span>Easy</span>.</h1>
-          </div>
-        </div>
-       
-       
-      </main>
-    );
-  }
+export default function Home() {
+  const [projects, setProjects] = useState<Project[]>([]);
 
+  useEffect(() => {
+    // Fetching data asynchronously
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data.json"); // Assuming the JSON file is named data.json
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const fetchedProjects: Project[] = await response.json();
+        setProjects(fetchedProjects);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-export default Hub;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-center">
+        <TypewriterEffectSmoothDemo />
+        <br></br>
+        <br></br>
+        <br></br>
+        <CardHoverEffectDemo projects={projects} />
+      </div>
+    </div>
+  );
+}
+
+function CardHoverEffectDemo({ projects }: Props) {
+  return (
+    <div>
+      <div className="max-w-5xl mx-auto px-8">
+        <HoverEffect items={projects} />
+      </div>
+    </div>
+  );
+}
+function TypewriterEffectSmoothDemo() {
+  // A web-based service to trade items with security
+
+  const words = [
+    {
+      text: "A",
+    },
+    {
+      text: "web-based",
+    },
+    {
+      text: "service",
+    },
+    {
+      text: "to",
+    },
+    {
+      text: "trade",
+    },
+    {
+      text: "items",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "security.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        Temple Trading Hub
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <Link href={"/signup"}>
+          <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+            Sign Up
+          </button>
+        </Link>
+        <Link href={"/login"}>
+          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+            Log In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
