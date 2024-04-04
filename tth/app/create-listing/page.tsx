3001e3bfda859a99617ee1dece4aa43fa08b681a
Timothy <timkhumpan@gmail.com>
2024-04-04 16:29:09 -0400

    added a function to get Image URL

--- tth/app/create-listing/page.tsx
+++ tth/app/create-listing/page.tsx
@@ -1,32 +1,49 @@
 "use client";
 import { useState } from "react";
-import { ref, uploadBytes, StorageReference } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  StorageReference,
+} from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
-import { initializeApp } from "firebase/app";
-import {storage} from "../firebase"
- 
-// Your web app's Firebase configuration
+import { storage } from "../firebase";
 
 const CreateListing: React.FC = () => {
   const [imageUpload, setImageUpload] = useState<File | null>(null);
 
-  const uploadImage = () => {
+  const uploadImage = async () => {
     if (imageUpload == null) return;
     const imageRef: StorageReference = ref(
       storage,
       `images/${imageUpload.name + uuidv4()}`
     );
-    uploadBytes(imageRef, imageUpload).then(() => {
+
+    try {
+      // Upload image to storage
+      await uploadBytes(imageRef, imageUpload);
+
+      // Get download URL of the uploaded image
+      const imageUrl = await getDownloadURL(imageRef);
+      console.log("Image uploaded successfully. Download URL:", imageUrl);
+
+      // Optionally, you can use the download URL for further processing (e.g., saving it to a database)
+      // Handle your logic here...
+
+      // Alert user about successful upload
       alert("Image uploaded");
-    });
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      // Handle error
+    }
   };
 
   return (
     <div className="App">
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <br />
+      <br />
+      <br />
+      <br />
       <p>we need to add other things offer user items as well </p>
       <input
         type="file"
