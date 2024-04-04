2accf2b5b8fdf7712cf764445471e34607256a2e
RichBryan <bryanli20282gmail.com>
2024-04-03 22:16:36 -0400

    added the other routes for the app

---
+++ tth/app/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+    /// this is for dynamic routing
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ tth/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        Profile
+    </div>
+  )
+}
+
+export default Profile
