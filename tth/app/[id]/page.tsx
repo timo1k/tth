e554f1d787e61b612ed793e53dfc40509f19d4cf
RichBryan <bryanli20282gmail.com>
2024-04-04 11:19:55 -0400

    added dynamic pages

--- tth/app/[id]/page.tsx
+++ tth/app/[id]/page.tsx
@@ -1,10 +1,129 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-const page = () => {
-    /// this is for dynamic routing
-  return (
-    <div>page</div>
-  )
+import React from "react";
+import { BackgroundGradient } from "../../components/ui/background-gradient";
+import { IconAppWindow } from "@tabler/icons-react";
+import Image from "next/image";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs, DocumentData } from "firebase/firestore";
+import { Item } from "@radix-ui/react-select";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAnPs18NOhGcVCtgyrxlSRSj9ePqVMxJY4",
+    authDomain: "temple-trading-hub-tth.firebaseapp.com",
+    projectId: "temple-trading-hub-tth",
+    storageBucket: "temple-trading-hub-tth.appspot.com",
+    messagingSenderId: "64770184657",
+    appId: "1:64770184657:web:fa85fbd041ff27fb487cf0",
+  };
+
+// Initialize Firebase test
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// interface Project {
+//   title: string;
+//   description: string;
+//   link: string;
+//   email: string;
+// }
+
+interface Items {
+  id: string;
+  title: string;
+  link: string;
+  tag: string;
+  description: string;
+  user_id: string;
 }
 
-export default page
+// interface Props {
+//   projects: Items[];
+// }
+
+export default function Page({ params }: any) {
+  // const [projects, setProjects] = useState<Project[]>([]);
+  const [users, setUsers] = useState<Items[]>([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      const querySnapshot = await getDocs(collection(db, "Item"));
+      const usersData: Items[] = [];
+      querySnapshot.forEach((doc: DocumentData) => {
+        usersData.push({ id: doc.id, ...doc.data() });
+      });
+      console.log(usersData);
+      setUsers(usersData);
+    }
+
+    fetchItems();
+  }, []);
+
+  // useEffect(() => {
+  //   // Fetching data asynchronously
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("/data.json"); // Assuming the JSON file is named data.json
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch data");
+  //       }
+  //       const fetchedProjects: Project[] = await response.json();
+  //       setProjects(fetchedProjects);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // Filter projects based on the specific link
+  const id = `${params.id}`; // Change this to your desired link
+  const filteredItems = users.filter((item) => item.id === id);
+
+  if (filteredItems.length === 0) {
+    return <div>Loading...</div>; // Or handle the case when the item with the given ID is not found
+  }
+
+  const item = filteredItems[0];
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <BackgroundGradientDemo project={item} />
+    </div>
+  );
+}
+function BackgroundGradientDemo({ project }: { project: Items }) {
+  return (
+    <div className="max-w-sm rounded-[22px] overflow-hidden">
+      <BackgroundGradient className="p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={project.link}
+          alt="NOT FOUND"
+          height="400"
+          width="400"
+          className="object-cover"
+        />
+        <div className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          {project.title}
+        </div>
+
+        <div className="text-sm text-neutral-600 dark:text-neutral-400">
+          {project.description}
+        </div>
+        <br></br>
+        <div className="text-sm text-neutral-600 dark:text-neutral-400">
+          <h1>contant me: {project.user_id}</h1>
+        </div>
+
+        {/* <Link href={project.link}>LINKKKKKK</Link> */}
+      </BackgroundGradient>
+    </div>
+  );
+}
